eba2d3403a97f58ecfa67e540ee175696fd1f502
kmetop <119588911@qq.com>
2012-07-02 20:46:18 +0800

    jdemsy
    Signed-off-by: kmetop <119588911@qq.com>

---
+++ src/resources/jdemsy/src/demsy.alerts.js
@@ -0,0 +1,249 @@
+(function($, jDemsy) {
+	jDemsy.alerts = {
+		verticalOffset : -75,
+		horizontalOffset : 0,
+		repositionOnResize : true,// 浏览器窗口大小变化后是否重新计算提示框的位置？
+		overlayOpacity : .01,
+		overlayColor : '#FFF',
+		draggable : true,
+		dialogClass : null
+	}
+
+	/*
+	 * 显示提示框
+	 */
+	var _show = function(title, msg, value, type, callback) {
+		var beginTime = new Date().getTime();
+
+		_hide();
+		_overlay('show');
+
+		var $container = $('<div id="alerts_container"><h1 id="alerts_title"></h1><div id="alerts_content"><div class="alerts_logo"></div><div id="alerts_message"></div></div></div>').appendTo($("BODY"));
+
+		if (jDemsy.alerts.dialogClass)
+			$container.addClass(jDemsy.alerts.dialogClass);
+
+		// IE6 Fix
+		var pos = ($.browser.msie && parseInt($.browser.version) <= 6) ? 'absolute' : 'fixed';
+
+		$container.css({
+			position : pos,
+			zIndex : 99999,
+			padding : 0,
+			margin : 0
+		});
+
+		var $title = $("#alerts_title", $container).text(title);
+		var $content = $("#alerts_content", $container).addClass(type);
+		var $message = $("#alerts_message", $container).html(msg.replace(/\n/g, '<br />'));
+
+		$container.css({
+			minWidth : $container.outerWidth(),
+			maxWidth : $container.outerWidth()
+		});
+
+		_reposition();
+		_bindWindowResize(true);
+
+		switch (type) {
+		case 'confirm':
+			$message.after('<div id="alerts_panel"><input type="button" value="{0}" id="alerts_ok" /><input type="button" value="{1}" id="alerts_cancel" /></div>'.format(jDemsy.nls.ok, jDemsy.nls.cancel));
+			var $ok = $("#alerts_ok", $container);
+			var $cancel = $("#alerts_cancel", $container);
+			$ok.click(function() {
+				_hide();
+				if (callback)
+					callback(true);
+			}).focus().keypress(function(e) {
+				if (e.keyCode == jDemsy.keyCode.ENTER)
+					$ok.trigger('click');
+				if (e.keyCode == jDemsy.keyCode.ESC)
+					$cancel.trigger('click');
+			});
+			$cancel.click(function() {
+				_hide();
+				if (callback)
+					callback(false);
+			}).keypress(function(e) {
+				if (e.keyCode == jDemsy.keyCode.ENTER)
+					$ok.trigger('click');
+				if (e.keyCode == jDemsy.keyCode.ESC)
+					$cancel.trigger('click');
+			});
+
+			break;
+		case 'prompt':
+			$message.append('<br /><input type="text" size="30" id="alerts_prompt" />').after('<div id="alerts_panel"><input type="button" value="{0}" id="alerts_ok" /><input type="button" value="{1}" id="alerts_cancel" /></div>'.format(jDemsy.nls.ok, jDemsy.nls.cancel));
+			var $prompt = $("#alerts_prompt", $container).width($message.width());
+			var $ok = $("#alerts_ok", $container);
+			var $cancel = $("#alerts_cancel", $container);
+			$ok.click(function() {
+				var val = $prompt.val();
+				_hide();
+				if (callback)
+					callback(val);
+			}).keypress(function(e) {
+				if (e.keyCode == jDemsy.keyCode.ENTER)
+					$ok.trigger('click');
+				if (e.keyCode == jDemsy.keyCode.ESC)
+					$cancel.trigger('click');
+			});
+			$cancel.click(function() {
+				_hide();
+				if (callback)
+					callback(null);
+			}).keypress(function(e) {
+				if (e.keyCode == jDemsy.keyCode.ENTER)
+					$ok.trigger('click');
+				if (e.keyCode == jDemsy.keyCode.ESC)
+					$cancel.trigger('click');
+			});
+			if (value)
+				$prompt.val(value);
+			$prompt.focus().select();
+
+			break;
+		default:
+			$message.after('<div id="alerts_panel"><input type="button" value="{0}" id="alerts_ok" /></div>'.format(jDemsy.nls.ok));
+			var $ok = $("#alerts_ok", $container);
+			$ok.click(function() {
+				_hide();
+				if (callback)
+					callback(true);
+			}).focus().keypress(function(e) {
+				if (e.keyCode == jDemsy.keyCode.ENTER || e.keyCode == jDemsy.keyCode.ESC)
+					$ok.trigger('click');
+			});
+
+			break;
+		}
+
+		// Make draggable
+		if (jDemsy.alerts.draggable) {
+			try {
+				$container.draggable({
+					handle : $title
+				});
+				$title.css({
+					cursor : 'move'
+				});
+			} catch (e) {
+				alert(e);
+			}
+		}
+
+		jDemsy.log("创建提示框：耗时 {0}ms.", (new Date().getTime() - beginTime));
+	};
+
+	/*
+	 * 隐藏提示框
+	 */
+	var _hide = function() {
+		$("#alerts_container").remove();
+		_overlay('hide');
+		_bindWindowResize(false);
+	};
+
+	/*
+	 * 遮住窗口内容，即弹出提示框时，禁止点击。
+	 */
+	var _overlay = function(status) {
+		switch (status) {
+		case 'show':
+			_overlay('hide');
+			$('<div id="alerts_overlay"></div>').css({
+				position : 'absolute',
+				zIndex : 99998,
+				top : '0px',
+				left : '0px',
+				width : '100%',
+				height : $(document).height(),
+				background : jDemsy.alerts.overlayColor,
+				opacity : jDemsy.alerts.overlayOpacity
+			}).appendTo($("BODY"));
+
+			break;
+		case 'hide':
+			$("#alerts_overlay").remove();
+
+			break;
+		}
+	};
+
+	/*
+	 * 重新计算提示框在浏览器窗口中的位置
+	 */
+	var _reposition = function() {
+		var $container = $("#alerts_container");
+		var $window = $(window);
+		var top = (($window.height() / 2) - ($container.outerHeight() / 2)) + jDemsy.alerts.verticalOffset;
+		var left = (($window.width() / 2) - ($container.outerWidth() / 2)) + jDemsy.alerts.horizontalOffset;
+		if (top < 0)
+			top = 0;
+		if (left < 0)
+			left = 0;
+
+		// IE6 fix
+		if ($.browser.msie && parseInt($.browser.version) <= 6)
+			top = top + $window.scrollTop();
+
+		$container.css({
+			top : top + 'px',
+			left : left + 'px'
+		});
+		$("#alerts_overlay").height($(document).height());
+	};
+
+	/*
+	 * 绑定浏览器窗口 resize 事件：即浏览器窗口大小发生变化时，重新计算提示框位置。
+	 */
+	var _bindWindowResize = function(status) {
+		if (jDemsy.alerts.repositionOnResize) {
+			switch (status) {
+			case true:
+				$(window).bind('resize', _reposition);
+				break;
+			case false:
+				$(window).unbind('resize', _reposition);
+				break;
+			}
+		}
+	};
+
+	/*
+	 * 弹出只有OK按钮的提示框
+	 */
+	var _alert = function(message, title, type, callback) {
+		_show(title, message, null, type, function(result) {
+			if (callback)
+				callback(result);
+		});
+	};
+
+	// 公共函数
+	jError = function(message, title, callback) {
+		_alert(message, title || jDemsy.nls.error, "error", callback);
+	};
+	jWarn = function(message, title, callback) {
+		_alert(message, title || jDemsy.nls.warn, "warn", callback);
+	};
+	jInfo = function(message, title, callback) {
+		_alert(message, title || jDemsy.nls.info, "info", callback);
+	};
+	jSuccess = function(message, title, callback) {
+		_alert(message, title || jDemsy.nls.success, "success", callback);
+	};
+	jConfirm = function(message, title, callback) {
+		_show(title || jDemsy.nls.confirm, message, null, 'confirm', function(result) {
+			if (callback)
+				callback(result);
+		});
+	};
+	jPrompt = function(message, value, title, callback) {
+		_show(title || jDemsy.nls.prompt, message, value, 'prompt', function(result) {
+			if (callback)
+				callback(result);
+		});
+	};
+
+})(jQuery, jDemsy);
---
+++ src/resources/jdemsy/src/demsy.core.js
@@ -0,0 +1,297 @@
+/**
+ * 定义jDemsy类
+ */
+(function($) {
+	jError = jWarn = jInfo = jSuccess = alert;
+	jConfirm = confirm;
+	jPrompt = prompt;
+	jDemsy = {
+		_debug : true,
+		// 后台URL地址配置
+		urls : {
+			login : "/login/load"// 登录页面的urls地址
+		},
+		nls : {
+			login : "Login",
+		},
+		tpl : {},
+		// AJAX状态码配置
+		status : {
+			ok : 200,
+			error : 300,
+			timeout : 301
+		},
+		// 键盘编码
+		keyCode : {
+			ENTER : 13,
+			ESC : 27,
+			END : 35,
+			HOME : 36,
+			SHIFT : 16,
+			TAB : 9,
+			LEFT : 37,
+			RIGHT : 39,
+			UP : 38,
+			DOWN : 40,
+			DELETE : 46,
+			BACKSPACE : 8
+		},
+		// isOverAxis : function(x, ref, size) {
+		// return (x > ref) && (x < (ref + size));
+		// },
+		// isOver : function(y, x, top, left, height, width) {
+		// return this.isOverAxis(y, top, height) && this.isOverAxis(x, left,
+		// width);
+		// },
+		/**
+		 * 调试
+		 */
+		log : function(msg) {
+			if (this._debug) {
+				var args = Array.prototype.slice.call(arguments, 1);
+				if (typeof (console) != "undefined")
+					console.log(msg.format(args));
+			}
+		},
+		/**
+		 * 登录：弹出登录框
+		 */
+		login : function(title) {
+			if ($.pdialog && this.urls.login) {
+				$.pdialog.open(this.urls.login, "login", title || this.nls.login, {
+					mask : true,
+					width : 520,
+					height : 260
+				});
+			} else {
+				window.location = this.urls.login;
+			}
+		},
+		/**
+		 * 将字符串转换成JSON对象
+		 */
+		toJson : function(str) {
+			try {
+				if ($.type(str) == 'string')
+					return $.parseJSON('(' + str + ')');
+				else
+					return str;
+			} catch (e) {
+				return {};
+			}
+		},
+		/**
+		 * 通过AJAX访问URL出错后调用该函数显示错误信息。
+		 */
+		doAjaxError : function(xhr, ajaxOptions, thrownError) {
+			jError("<div>Http status: " + xhr.status + " " + xhr.statusText + "</div>" + "<div>ajaxOptions: " + ajaxOptions + "</div>" + "<div>thrownError: " + thrownError + "</div>" + "<div>" + xhr.responseText + "</div>");
+		},
+		/**
+		 * AJAX访问URL成功后调用该函数处理返回结果
+		 */
+		doAjaxSuccess : function(json) {
+			// 返回值不是一个JSON对象
+			if (json.statusCode === undefined && json.message === undefined) {
+				return jError(json);
+			}
+			// 返回值是错误信息
+			if (json.statusCode == this.status.error && json.message) {
+				jError(json.message);
+			}
+			// 返回值是登录超时
+			else if (json.statusCode == this.status.timeout) {
+				this.login();
+			} else if (json.message) {
+				jSuccess(json.message);
+			}
+		}
+	};
+})(jQuery);
+/**
+ * 扩展jQuery对象方法
+ */
+(function($, jDemsy) {
+	$.fn.extend({
+		/**
+		 * 通过AJAX访问URL
+		 * <p>
+		 * 1. 如果返回内容是一个JSON对象：且访问的URL需要登录认证，则未登录或会话过期时将会自动弹出登录框；
+		 * <p>
+		 * 2. 如果返回内容是一个JSON对象：且访问URL出现错误，则将弹出错误信息提示框；
+		 * <p>
+		 * 3. 如果返回内容不是一个JSON对象：则将返回内容作为当前元素的HTML内容并初始化UI；
+		 */
+		dajax : function(options) {
+			if ($.fn.xheditor) {
+				$("textarea.editor", this).xheditor(false);
+			}
+
+			$.ajax({
+				type : options.type || 'GET',
+				url : options.url,
+				data : options.data,
+				cache : false,
+				success : function(response) {
+					// 将内容转换成JSON对象
+					var json = jDemsy.toJson(response);
+
+					// 返回值是“会话过期/未登录”：弹出登录框
+					if (json.statusCode == jDemsy.status.timeout) {
+						jDemsy.login(json.message);
+					}
+					// 返回值是“错误信息”：弹出错误提示框
+					else if (json.statusCode == jDemsy.status.error) {
+						jError(json.message);
+					}
+					// 返回值是“HTML内容”：将其作为元素的HTML内容
+					else {
+						$this.html(response).demsyInit();
+						if ($.isFunction(options.callback))
+							options.callback(response);
+					}
+				},
+				error : jDemsy.doAjaxError
+			});
+		},
+		/**
+		 * 发送GET请求到指定的URL地址加载内容，该方法是demsyAjax的简化用法
+		 */
+		dget : function(url, data, callback) {
+			this.dajax({
+				url : url,
+				data : data,
+				callback : callback
+			});
+		},
+		/**
+		 * 发送POST请求到指定的URL地址加载内容，该方法是demsyAjax的简化用法
+		 */
+		dpost : function(url, data, callback) {
+			this.dajax({
+				url : url,
+				type : "POST",
+				data : data,
+				callback : callback
+			});
+		},
+		/**
+		 * 初始化元素内容中的UI
+		 */
+		dinit : function() {
+			return this.each(function() {
+				if ($.isFunction(initUI))
+					initUI(this);
+			});
+		},
+		/**
+		 * 校正元素高度为指定对象的高度
+		 */
+		dlayout : function($refBox) {
+			return this.each(function() {
+				var $this = $(this);
+				if (!$refBox)
+					$refBox = $this.parents("div.layoutBox:first");
+				var iRefH = $refBox.height();
+				var iLayoutH = parseInt($this.attr("layoutH"));
+				var iH = iRefH - iLayoutH > 50 ? iRefH - iLayoutH : 50;
+
+				if ($this.isTag("table")) {
+					$this.removeAttr("layoutH").wrap('<div layoutH="' + iLayoutH + '" style="overflow:auto;height:' + iH + 'px"></div>');
+				} else {
+					$this.height(iH).css("overflow", "auto");
+				}
+			});
+		},
+		/**
+		 * 鼠标移动到元素上时套用指定的CSS
+		 */
+		hoverClass : function(className) {
+			var _className = className || "hover";
+			return this.each(function() {
+				$(this).hover(function() {
+					$(this).addClass(_className);
+				}, function() {
+					$(this).removeClass(_className);
+				});
+			});
+		},
+		/**
+		 * 元素获得焦点时套用指定的CSS
+		 */
+		focusClass : function(className) {
+			var _className = className || "textInputFocus";
+			return this.each(function() {
+				$(this).focus(function() {
+					$(this).addClass(_className);
+				}).blur(function() {
+					$(this).removeClass(_className);
+				});
+			});
+		},
+		/**
+		 * 输入框获得焦点时显示提示信息
+		 */
+		altBox : function() {
+			return this.each(function() {
+
+				var $this = $(this);
+
+				function getAltBox() {
+					return $this.parent().find("label.alt");
+				}
+				function altBoxCss(opacity) {
+					var position = $this.position();
+					return {
+						width : $this.width(),
+						top : position.top + 'px',
+						left : position.left + 'px',
+						opacity : opacity || 1
+					}
+				}
+				if (getAltBox().size() < 1) {
+					if (!$this.attr("id"))
+						$this.attr("id", $this.attr("name") + "_" + Math.round(Math.random() * 10000));
+					var $label = $('<label class="alt" for="' + $this.attr("id") + '">' + $this.attr("alt") + '</label>').appendTo($this.parent());
+
+					$label.css(altBoxCss(1));
+					if ($this.val())
+						$label.hide();
+				}
+
+				$this.focus(function() {
+					getAltBox().css(altBoxCss(0.3));
+				}).blur(function() {
+					if (!$(this).val())
+						getAltBox().show().css("opacity", 1);
+				}).keydown(function() {
+					getAltBox().hide();
+				});
+			});
+		},
+		/**
+		 * 判断元素是不是指定的HTML标签
+		 */
+		isTag : function(tn) {
+			if (!tn)
+				return false;
+			return this.tagName.toLowerCase() == tn.toLowerCase() ? true : false;
+		},
+		/**
+		 * 判断当前元素是否已经绑定某个事件
+		 */
+		hasEvent : function(type) {
+			var _events = $(this).data("events");
+			return _events && type && _events[type];
+		},
+		/**
+		 * 输出firebug日志
+		 */
+		log : function(msg) {
+			return this.each(function() {
+				if (console)
+					console.log("%s: %o", msg, this);
+			});
+		}
+	});
+
+})(jQuery, jDemsy);
